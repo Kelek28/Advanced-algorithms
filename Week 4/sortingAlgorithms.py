535109a1a47c79825a33b3c125e2861e273d5e47
Kacper Drupisz <kacper.drupisz@gmail.com>
2022-02-23 12:36:01 +0000

    Sorting, binary search

--- Week 4/sortingAlgorithms.py
+++ Week 4/sortingAlgorithms.py
@@ -1,5 +1,7 @@
-import random
 import time
+import random
+import sys
+sys.setrecursionlimit(10000)
 
 
 def bubble_sort(list):
@@ -26,28 +28,31 @@ def selection_sort(list):
 
 def merge_sort(list):
     # calculate the length of the list as n
-    if len(list) <= 1: return list
+    if len(list) <= 1:
+        return list
     # middle of the list
     mid = len(list)//2
     left_list = merge_sort(list[:mid])
     right_list = merge_sort(list[mid:])
     return merge(left_list, right_list)
 
+
 def merge(left, right):
-    output=[]
+    output = []
     # create two counters for two lists as lc and rc
-    lc, rc = 0,0
+    lc, rc = 0, 0
     while lc < len(left) and rc < len(right):
         if left[lc] < right[rc]:
             output.append(left[lc])
             lc += 1
         else:
             output.append(right[rc])
-            rc += 1 
+            rc += 1
         # add the unfinished part of left or right into the output
     output.extend(left[lc:])
     output.extend(right[rc:])
     return output
+
 
 def quick_sort(list):
     if len(list) < 2:
@@ -56,50 +61,93 @@ def quick_sort(list):
         left = []
         right = []
         # we use the first element as the pivot
-        for i in range(1,len(list)):
-            if list[i]>list[0]:
+        for i in range(1, len(list)):
+            if list[i] > list[0]:
                 right.append(list[i])
             else:
                 left.append(list[i])
     return(quick_sort(left) + list[:1] + quick_sort(right))
 
+
 def classic_quick_sort(list):
     partition(list, 0, len(list)-1)
     return list
 
+
 def partition(list, lb, rb):
-    if rb-lb<=1: # one element only or an empty list
+    if rb-lb <= 1:  # one element only or an empty list
         return
     else:
-    # take the most right element as the pivot
-    #create new bounds
+        # take the most right element as the pivot
+        pivot = rb
+        # create new bounds
         left = lb
         right = rb-1
-        pivot = right
-        #compare the elements with the pivot from the left2right first
+        # compare the elements with the pivot from the left2right first
         left2right = True
-        while left <= right: 
+        while left <= right:
             if left2right:
                 if list[left] > list[pivot]:
                     # swap the values of left and pivot
+                    temp = list[pivot]
+                    list[pivot] = list[left]
+                    list[left] = temp
                     # set new pivot as left
-                    left2right = False # compare from right to left
+                    pivot = left
+                    left2right = False  # compare from right to left
                 left += 1
             else:
                 if list[right] < list[pivot]:
                     # swap the values of right and pivot
+                    temp = list[pivot]
+                    list[pivot] = list[right]
+                    list[right] = temp
                     # set new pivot as right
+                    pivot = right
                     left2right = True
                 right -= 1
+
         partition(list, lb, pivot-1)
         partition(list, pivot, rb)
 
 
+def binary_search(element, list, lower, upper):
+    if upper >= lower:
+        mid = lower + (upper-lower)//2
+        if list[mid] == element:
+            return True
+        if list[mid] < element:
+            lower = mid+1
+            return binary_search(element, list, lower, upper)
+        if list[mid] > element:
+            upper = mid-1
+            return binary_search(element, list, lower, upper)
+    else:
+        return -1
+
+
+def binary_search_iterartion(element, list):
+    lower = 0
+    upper = len(list)-1
+    while lower <= upper:
+        mid = lower + (upper - lower) // 2
+        if list[mid] == element:
+            return element
+        if list[mid] < element:
+            lower = mid
+        if list[mid] > element:
+            upper = mid
+    else:
+        return -1
+
+
 def main():
-    f = open('numbers.txt', 'r')
-    line = f.read()
-    list = line.split(' ')
-    # # Bubble sort algorithm
+    # f = open('numbers.txt', 'r')
+    # line = f.read()
+    # list = line.split(' ')
+    # list = [int(x) for x in list]
+    list = [int(x) for x in range(100000000)]
+    # # # Bubble sort algorithm
     # start = time.time()
     # bubble_sort(list)
     # end = time.time()
@@ -119,11 +167,13 @@ def main():
     # quick_sort(list)
     # end = time.time()
     # print("The elapsed time is Quick sort not-in-place " + str(end-start))
-    # # Quick sort  in place algorithm
-    start = time.time()
-    classic_quick_sort(list)
-    end = time.time()
-    print("The elapsed time is Quick sort in-place " + str(end-start))
+    # # # Quick sort  in place algorithm
+    # start = time.time()
+    # classic_quick_sort(list)
+    # end = time.time()
+    # print("The elapsed time is Quick sort in-place " + str(end-start))
+    print(binary_search(2, list, 0, len(list)-1))
+    print(binary_search_iterartion(2, list))
 
 
 if __name__ == '__main__':
